8369a0ef09b383edf720ff6efe94935298f4b851
Your Name <you@example.com>
2023-09-19 20:49:27 -0300

    ApiPastelariaRest

--- mod_cliente/ClienteModel.py
+++ mod_cliente/ClienteModel.py
@@ -10,7 +10,7 @@ class ClienteDB(db.Base):
     cpf = Column(CHAR(11), unique=True, nullable=False)
     telefone = Column(CHAR(11), nullable=False)
     compra_fiado = Column(Integer, nullable=True)
-    dia_fiado = Column(Date, nullable=True)
+    dia_fiado = Column(Integer, nullable=True)
     senha = Column(VARCHAR(200), nullable=False)
 
     def __init__(self, id_cliente, nome, cpf, telefone, compra_fiado, dia_fiado, senha):
--- mod_produto/ProdutoModel.py
+++ mod_produto/ProdutoModel.py
@@ -8,7 +8,7 @@ class ProdutoDB(db.Base):
     id_produto = Column(Integer, primary_key=True, autoincrement=True, index=True)
     nome = Column(VARCHAR(100), nullable=False)
     descricao = Column(VARCHAR(100), nullable=False)
-    foto = Column(Integer, unique=True, nullable=False)
+    foto = Column(VARCHAR(100), unique=True, nullable=False)
     valor_unit = Column(Integer, nullable=False)
 
     def __init__(self, id_produto, nome, descricao, foto, valor_unit):
